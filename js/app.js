8b3a0e593e0a471b1934d42ae1e86972bd2103fb
WhiteDeath820 <allensam0@gmail.com>
2015-10-23 00:29:31 -0600

    heyyyyyy

--- js/app.js
+++ js/app.js
@@ -15,34 +15,58 @@ yeezyweb.config(function ($stateProvider, $urlRouterProvider, $locationProvider)
 
     //states
     $stateProvider
+        // yeezy home
         .state('yeezyhome', {
             url: '',
             templateUrl: '/yeezystates/yeezyhome.html',
             controller: 'yeezyHomeYeezyCtrl'
         })
+        // abstract for /learn sidebar
         .state('yeezysidebar', {
             abstract: yeezytrue,
             url: '/learn',
             templateUrl: '/yeezystates/yeezysidebar.html'
         })
+        // learn intro
         .state('yeezysidebar.yeezyintro', {
             url: '/angular',
-            templateUrl: '/yeezystates/yeezyintro.html',
-            controller: 'yeezyAngularYeezyCtrl'
+            templateUrl: '/yeezypartials/yeezyintro.html'
         })
+        // learn angular
         .state('yeezysidebar.yeezyangular', {
             url: '/angular',
-            templateUrl: '/yeezystates/yeezyangular.html',
-            controller: 'yeezyAngularYeezyCtrl'
+            templateUrl: '/yeezypartials/yeezyangular.html'
         })
+        // learn html
         .state('yeezysidebar.yeezyhtml', {
             url: '/html',
-            templateUrl: '/yeezystates/yeezyhtml.html',
-            controller: 'yeezyHtmlYeezyCtrl'
+            templateUrl: '/yeezypartials/yeezyhtml.html'
         })
+        // learn php
+        .state('yeezysidebar.yeezyphp', {
+            url: '/php',
+            templateUrl: '/yeezypartials/yeezyphp.html'
+        })
+        // learn css
+        .state('yeezysidebar.yeezycss', {
+            url: '/css',
+            templateUrl: '/yeezypartials/yeezycss.html'
+        })
+        // learn nodejs
+        .state('yeezysidebar.yeezynode', {
+            url: '/nodejs',
+            templateUrl: '/yeezypartials/yeezynode.html'
+        })
+        // learn ruby
+        .state('yeezysidebar.yeezyruby', {
+            url: '/ruby',
+            templateUrl: '/yeezypartials/yeezyruby.html'
+        })
+        // yeezy vision
         .state('yeezyvision', {
             url: '/vision',
             templateUrl: '/yeezystates/yeezyvision.html',
             controller: 'yeezyHtmlYeezyCtrl'
-        })
+        });
+    $urlRouterProvider.otherwise('/');
 });
