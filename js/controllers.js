06c9234a1d669a382e89c15e805eeaac573aa945
WhiteDeath820 <allensam0@gmail.com>
2015-10-22 20:09:51 -0600

    started learn yeezy

--- js/controllers.js
+++ js/controllers.js
@@ -5,7 +5,16 @@ yeezyCtrl.controller('yeezyHomeYeezyCtrl', function () {
 });
 
 yeezyCtrl.controller('yeezyAngularYeezyCtrl', function () {
+    this.angularinfo = yeezyangular;
+    var yeezyangular = [{
+        name: 'Yeezy App',
+        description: "f"
 
+    }, {
+        name: 'Yeezy Controllers',
+        description: "f"
+
+    }]
 });
 
 yeezyCtrl.controller('yeezyHtmlYeezyCtrl', function () {
