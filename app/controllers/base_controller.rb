d002563ec74dec5429810973650219038fff78ac
WhiteDeath820 <allensam0@gmail.com>
2015-12-16 21:36:44 -0700

    finishdocumentating

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,6 +2,7 @@
 class BaseController < ApplicationController
 
   # index is the current landing page and index for app
+  # @see http://localhost:3000 home page
   def index
   end
 
--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,23 +1,30 @@
+# Controller for tutorials
 class TutorialsController < ApplicationController
 
   # index is the introduction page to display all things we have
+  # @see http://localhost.com/tutorials Tutorial landing page
   def index
   end
 
-  # html page maybe combine with css?
+  # html page maybe combine with css
+  # @see http://localhost:3000/tutorials/html html tutorial
   def html
   end
 
   # angular page possibly have javascript and then angular extend on it
+  # @author Sam Allen
+  # @see http://localhost:3000/tutorials/angular angular tutorial
   def angular
   end
-  
+
   # combine with html?
+  # @see http://localhost:3000/tutorials/css css tutorial
   def css
   end
 
   # This needs to be changed to javascript to follow descriptive and readable code style
   # also maybe have this as base of javascript with angular stuff and jquery and react?
+  # @see http://localhost:3000/tutorials/js javascript tutorial
   def js
   end
 
