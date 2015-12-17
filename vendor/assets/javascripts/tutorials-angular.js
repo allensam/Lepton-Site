5eea1d7b0019ada384a198a985b4b36edf651bba
WhiteDeath820 <allensam0@gmail.com>
2015-12-17 14:04:16 -0700

    Moved javascript to vendors/assets

---
+++ vendor/assets/javascripts/tutorials-angular.js
@@ -0,0 +1,14 @@
+// initiates angular app
+var app = angular.module('app', []);
+// controller for this page
+app.controller('testing', function($scope) {
+  //binding text area
+$scope.angular_example = "$scope.showNewDiv = true;"
+
+  //method evulates code in binded area
+  function evaluatetor(inputcode) {
+    eval(inputcode);
+  }
+  var input = $scope.angular_example;
+  evaluatetor(input);
+});
