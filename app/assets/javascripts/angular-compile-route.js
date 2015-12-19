ea972c8950134dbf60ad6b5983b3bc757462abc1
WhiteDeath820 <allensam0@gmail.com>
2015-12-19 16:50:51 -0700

    got rid of errors

--- app/assets/javascripts/angular-compile-route.js
+++ app/assets/javascripts/angular-compile-route.js
@@ -3,4 +3,6 @@
 //= require tutorials-angular.js
 // this is to load the angular editors in the angular page
 //= require ace/ace.js
+//= require ace/theme-monokai.js 
+//= require ace/mode-javascript.js
 //= require editors/angular-page-editors.js
