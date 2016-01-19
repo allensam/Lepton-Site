a4501cac018772b11d7570564764f82555db0a28
WhiteDeath820 <allensam0@gmail.com>
2016-01-19 15:13:11 -0700

    made error handling better

--- app/assets/javascripts/tutorials-angular.js
+++ app/assets/javascripts/tutorials-angular.js
@@ -2,21 +2,24 @@
 // initiates angular app
 var angular_page = angular.module('angular_page', []);
 /** controller for this page
-* @param $scope [string] injects angular scope
-*/
+ * @param $scope [string] injects angular scope
+ */
 angular_page.controller('testing', function($scope) {
   $scope.showNewDiv = false;
-  $scope.testing = "no errors"
+  $scope.testing = "no errors";
   $scope.runCode = function(name) {
+
+    var passed = false;
     var input = eval(name + ".return_text_in_editor();");
     try {
-      eval(input);
-    }
-    catch(err) {
-      if (err != null) {
-        $scope.testing = err.message;
-        console.log(err.message);
+      eval(input + "passed = true;")
+    } catch(e) {
+      $scope.testing = e.message;
+    } finally {
+      if (passed == true) {
+        $scope.testing = "No erros! :)"
       }
     }
+
   }
 });
