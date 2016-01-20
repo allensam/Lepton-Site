86f30342f9ae5abea00fcfa5a5a5d0a9af13bb88
WhiteDeath820 <allensam0@gmail.com>
2016-01-19 18:04:17 -0700

    removed angular compile

--- app/assets/javascripts/compile-routes/angular-compile-route.js
+++ app/assets/javascripts/compile-routes/angular-compile-route.js
@@ -1,5 +1,5 @@
 // This file is so that angular is only loaded on one view
-//= require angular.min.js
+//= require angular.js
 //= require tutorials-angular.js
 // this is to load the angular editors in the angular page
 //= require editors/angular-page.js
