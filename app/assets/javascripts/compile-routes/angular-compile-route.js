540fdbbdf73dc66e0cd9a2b5b4ac152e2cd87d11
WhiteDeath820 <allensam0@gmail.com>
2016-01-23 22:11:14 -0700

    fixed angular load twice problem

--- app/assets/javascripts/compile-routes/angular-compile-route.js
+++ app/assets/javascripts/compile-routes/angular-compile-route.js
@@ -1,5 +1,2 @@
-// This file is so that angular is only loaded on one view
-//= require angular.js
-//= require tutorials-angular.js
 // this is to load the angular editors in the angular page
 //= require editors/angular-page.js
