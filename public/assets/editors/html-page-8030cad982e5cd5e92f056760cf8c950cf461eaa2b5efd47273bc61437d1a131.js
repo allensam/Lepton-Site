063b42c095176bfb57ae22664ebd6fec1c3352ad
WhiteDeath820 <allensam0@gmail.com>
2016-01-24 17:17:49 -0700

    wrong env precompile

---
+++ public/assets/editors/html-page-8030cad982e5cd5e92f056760cf8c950cf461eaa2b5efd47273bc61437d1a131.js
@@ -0,0 +1,1 @@
+"use strict";var htmlExample=new Editor("htmlExample","html");htmlExample.set_editor_text('\n<div id="firstDiv">\n  Hello!\n</div>\n<div id="secondDiv">\n  These are dividers =\n</div>\n<style>\n  #firstDiv {\n    height: 30px;\n    width: 200px;\n    padding: 10px;\n    background-color: #333;\n  }\n  #secondDiv {\n    \n  }\n  ');
