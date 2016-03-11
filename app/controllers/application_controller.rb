11d45844a4f465cebf0aef70023db1ec186f2549
WhiteDeath820 <allensam0@gmail.com>
2016-03-10 18:33:55 -0700

    finished responsive mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,26 +7,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :prepare_for_mobile
-
-  private
-
-  def mobile_device?
-    if session[:mobile_param]
-      session[:mobile_param] == '1'
-    else
-      request.user_agent =~ /Mobile|webOS/
-    end
-  end
-  helper_method :mobile_device?
-
-  def mobile!
-    redirect_to root_path(:mobile => 1)
-  end
-  helper_method :mobile!
-
-  def prepare_for_mobile
-    session[:mobile_param] = params[:mobile] if params[:mobile]
-    request.format = :mobile if mobile_device?
-  end
 end
