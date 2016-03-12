58679592b25617d2bfaf141c75920036b9e7a8e5
WhiteDeath820 <allensam0@gmail.com>
2016-03-11 22:32:00 -0700

    added a list of todo's

--- spec/javascripts/editor_spec.js
+++ spec/javascripts/editor_spec.js
@@ -1,5 +1,5 @@
 fixture.load('editor.html.erb', append = false);
-
+//TODO finish writting tests after tabs class is finished
 import {Editor} from 'editors/editor.js'
 describe("Editor", function() {
     const editor = new Editor('test-editor', 'javascript', true);
