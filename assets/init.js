d3fc035566c44009405f4b4f981de92dd885d753
WhiteDeath820 <allensam0@gmail.com>
2016-03-12 22:04:14 -0700

    a lot of work on initiazation

--- assets/init.js
+++ assets/init.js
@@ -1,35 +1,51 @@
 ace.config.set("basePath", "../assets/ace_editor");
 import {Editor} from 'editors/editor.js';
-let editors = document.querySelectorAll('.tab');
-
+let editors = document.querySelectorAll('.e');
+var editorObj = {}
 (function (e, d) {
     for (let i = 0; i < e.length; i++) {
         let langs = e[i].getAttribute('data-langs');
-        let id = e[i].id;
+        let el = e[i];
         if (langs.length > 1) {
             let names = e[i].getAttribute('data-names');
-            thing(id,langs,names);
+            handleExceptions(el,langs,names);
         } else {
-            thing(id,langs,null);
+            handleExceptions(el,langs,null);
         }
+        generateCSS(el, langs);
     }
 
-    function thing(id, langs, names) {
+    function generateCSS(el, langs) {
+        let id = el.id;
+        var style = document.createElement('style');
+        style.type = 'text/css';
+        for (let i = 0; i < langs.length; i++) {
+            style.innerHTML = `##{id}#{langs[i]} { position: absolute; }`;
+        }
+
+        document.getElementsByTagName('head')[0].appendChild(style);
+
+        document.getElementById('someElementId').className = 'cssClass';
+    }
+
+    function handleExceptions(el, langs, names) {
         let string = langs.toString();
-        if (string.match(/react/i)) {
+        let exceptions = {};
+        if (string.match(/react|rhtml|reahtml|angular|ahtml|typescript/i)) {
+            let exceptionsList = ['react','rhtml','reahtml','angular','ahtml','typescript'];
+            for (let i = 0; i < exceptionsList; i++) {
+                let index = langs.findIndex(x => x == i);
+                if (index != -1) {
+                    exceptions[i] = index;
+                }
+            }
+        }
+        startGeneration(el, langs, names, exceptions);
+    }
 
-        } else if (string.match(/typescript/i)) {
-
-        } else if (string.match(/rhtml/i)) {
-
-        } else if (string.match(/ahtml/i)) {
-
-        } else if (string.match(/reahtml/i)) {
-
-        } else if (string.match(/angular/i)) {
-
-        } else {
-
+    function startGeneration(el, langs, names, exceptions) {
+        if (langs.length === 1 && names === null && exceptions === null) {
+            editorObj[el.id] = new Editor(el.id, langs[1])
         }
     }
-})(editors, document);
+})(editors, document, editorObj);
