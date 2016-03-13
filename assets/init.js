ce562656600ad7a2b005f31765ea758f3d30942c
WhiteDeath820 <allensam0@gmail.com>
2016-03-12 18:38:35 -0700

    so much annoying code

--- assets/init.js
+++ assets/init.js
@@ -1,3 +1,35 @@
 ace.config.set("basePath", "../assets/ace_editor");
 import {Editor} from 'editors/editor.js';
-//TODO create the runner here
+let editors = document.querySelectorAll('.tab');
+
+(function (e, d) {
+    for (let i = 0; i < e.length; i++) {
+        let langs = e[i].getAttribute('data-langs');
+        let id = e[i].id;
+        if (langs.length > 1) {
+            let names = e[i].getAttribute('data-names');
+            thing(id,langs,names);
+        } else {
+            thing(id,langs,null);
+        }
+    }
+
+    function thing(id, langs, names) {
+        let string = langs.toString();
+        if (string.match(/react/i)) {
+
+        } else if (string.match(/typescript/i)) {
+
+        } else if (string.match(/rhtml/i)) {
+
+        } else if (string.match(/ahtml/i)) {
+
+        } else if (string.match(/reahtml/i)) {
+
+        } else if (string.match(/angular/i)) {
+
+        } else {
+
+        }
+    }
+})(editors, document);
