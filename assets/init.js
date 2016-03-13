0956e3dea4c196af4fa4b52bf3609e2f593d9222
WhiteDeath820 <allensam0@gmail.com>
2016-03-12 23:33:57 -0700

    more work

--- assets/init.js
+++ assets/init.js
@@ -1,7 +1,7 @@
 ace.config.set("basePath", "../assets/ace_editor");
 import {Editor} from 'editors/editor.js';
 let editors = document.querySelectorAll('.e');
-var editorObj = {}
+var editorObj = {};
 (function (e, d) {
     for (let i = 0; i < e.length; i++) {
         let langs = e[i].getAttribute('data-langs');
@@ -17,15 +17,15 @@ var editorObj = {}
 
     function generateCSS(el, langs) {
         let id = el.id;
-        var style = document.createElement('style');
+        var style = d.createElement('style');
         style.type = 'text/css';
         for (let i = 0; i < langs.length; i++) {
             style.innerHTML = `##{id}#{langs[i]} { position: absolute; }`;
         }
 
-        document.getElementsByTagName('head')[0].appendChild(style);
+        d.getElementsByTagName('head')[0].appendChild(style);
 
-        document.getElementById('someElementId').className = 'cssClass';
+        d.getElementById('someElementId').className = 'cssClass';
     }
 
     function handleExceptions(el, langs, names) {
